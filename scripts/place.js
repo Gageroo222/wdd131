a74a7756377728216b78995bd834c116b138fcca
gageroo222 <gage.carter2003@gmail.com>
2024-06-19 13:09:05 -0700

    place website finished

--- scripts/place.js
+++ scripts/place.js
@@ -6,3 +6,28 @@ document.addEventListener('DOMContentLoaded', function() {
     var lastModifiedElement = document.getElementById('lastModified');
     var lastModifiedDate = document.lastModified;
     lastModifiedElement.textContent = 'Last Modified: ' + lastModifiedDate;});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    function calculateWindChill(tempF, windSpeed) {
+        if (tempF <= 50 && windSpeed > 3) {
+            const windChill = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * tempF * Math.pow(windSpeed, 0.16));
+            return windChill.toFixed(2);
+        } else {
+            return 'N/A';
+        }
+    }
+    
+    function displayWindChill() {
+        const tempElement = document.getElementById('temperature');
+        const windSpeedElement = document.getElementById('wind-speed');
+        const windChillElement = document.getElementById('wind-chill');
+    
+        const temperature = parseFloat(tempElement.textContent);
+        const windSpeed = parseFloat(windSpeedElement.textContent);
+    
+        const windChill = calculateWindChill(temperature, windSpeed);
+        windChillElement.textContent = windChill;
+    }
+    
+    displayWindChill();
+});
